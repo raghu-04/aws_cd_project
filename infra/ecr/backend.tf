e637f9f0e1da49b7a96d9d91f827acfb15407d64
raghu-04 <121910316014@gitam.in>
2026-05-06 21:24:40 -0500

    restart

--- infra/ecr/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "online-botique-tfstatefile-raghu0410"
-    key = "online-boutique/terraform.tfstate"
-    region = "us-east-1"
-    encrypt = true
-    dynamodb_table = "online-boutique-tfstatefile-lock"
-  }
-}
--- infra/ecr/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "ecr_repo_urls" {
-    value = { for k, r in aws_ecr_repository.svc : k => r.repository_url }
-}
